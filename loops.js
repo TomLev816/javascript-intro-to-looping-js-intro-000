183397040ffefeb98f3b7aff3a919d1ba53ef632
Thomas Lever <thomaslever508@gmail.com>
2018-06-12 14:10:34 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,9 @@
+function forLoop(array){
+  for (let i = 1; i<25;i++){
+    if (i === 1){
+      return '${array} I am 1 strange loop.'
+    } else {
+      return '${array} I am ${i} strange loops.'
+    }
+  }
+}
