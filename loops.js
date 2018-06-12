682a58ccb96e5d0bb0d768e86028279671243ddd
Thomas Lever <thomaslever508@gmail.com>
2018-06-12 14:10:55 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,18 @@
 function forLoop(array){
-  for (let i = 1; i<25;i++){
+  for (let i = 0; i<25;i++){
     if (i === 1){
-      return '${array} I am 1 strange loop.'
+      array.push('I am 1 strange loop.')
     } else {
-      return '${array} I am ${i} strange loops.'
+      array.push('I am ${i} strange loops.')
     }
   }
+return array
+}
+
+function whileLoop(n){
+  while (n>0){ 
+    --n
+    console.log(n)
+  }
+  return 'done'
 }
